f1bd4e1ff1e1f2cba722c881ccfee24335605a78
DevvysEngines <maskertacker@gmail.com>
2025-07-12 23:14:36 -0700

    Smart System Upgrade-Smart Nodes!
    Added Smart Nodes that support both traditional parameters and a single object parameter for configuration. This makes node creation more flexible, user-friendly, and accessible for advanced features.

--- Unlocked/Engine/node.js
+++ Unlocked/Engine/node.js
@@ -1,12 +1,24 @@
 import { utils } from "./utils.js";
 
 export class node{
-    constructor(Name=`node`,tags=[],onApply=()=>{},update=()=>{},onFinished=()=>{}){
-        this.onApply = onApply;
-        this.update = update;
-        this.onFinished = onFinished;
-        this.Name = Name;
-        this.tags = utils.normalizePath(tags);
+    #defaults = {
+        Name:`node`
+        ,tags:[]
+        ,onApply:function(){}
+        ,update:function(){}
+        ,onFinished:function(){}
+        ,type:`node`
+    }
+    constructor(Name, tags, onApply, update, onFinished){
+        if (typeof Name !== `object`){Name={Name, tags, onApply, update, onFinished, type:`node`};}
+        Object.assign(
+            this
+            ,{...this.#defaults}
+            ,Object.fromEntries(
+                Object.entries(Name).filter(([_, v]) => v !== undefined)
+            )
+        );
+        this.tags = utils.normalizePath(this.tags);
         this.type = `node`;
     }
 }
